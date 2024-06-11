7266330b2a8ae6bdf0ea5dacdf53da637b43c4e3
Cedric LE CAM <cedric.lecam@sii.fr>
2024-06-11 11:23:37 +0200

    Add tests for GetFileSize

--- test/basic_test.cpp
+++ test/basic_test.cpp
@@ -47,9 +47,50 @@ TEST(GCSDriverTest, Disconnect)
 	ASSERT_FALSE(driver_isConnected());
 }
 
-TEST(GCSDriverTest, GetFileSize)
+class GCSDriverTestFixture : public ::testing::Test
 {
-	ASSERT_FALSE(driver_connect());
-	ASSERT_EQ(driver_getFileSize("gs://data-test-khiops-driver-gcs/khiops_data/samples/Adult/Adult.txt"), 5585568);
-	ASSERT_FALSE(driver_disconnect());
+public:
+	static void SetUpTestSuite()
+	{
+		ASSERT_FALSE(driver_connect());
+	}
+
+	static void TearDownTestSuite()
+	{
+		ASSERT_FALSE(driver_disconnect());
+	}
+};
+
+TEST_F(GCSDriverTestFixture, FileExists)
+{
+	//single file
+	ASSERT_TRUE(driver_fileExists("gs://data-test-khiops-driver-gcs/khiops_data/bq_export/Adult/Adult-split-000000000000.txt"));
+
+	//file glob
+	ASSERT_TRUE(driver_fileExists("gs://data-test-khiops-driver-gcs/khiops_data/split/Adult_subsplit/**"));
+
+	//with dir
+	ASSERT_FALSE(driver_fileExists("gs://data-test-khiops-driver-gcs/khiops_data/bq_export/Adult/"));
+}
+
+TEST_F(GCSDriverTestFixture, GetFileSize)
+{
+	constexpr const char* test_file = "gs://data-test-khiops-driver-gcs/khiops_data/samples/Adult/Adult.txt";
+	constexpr long long expected_size{ 5585568 };
+	ASSERT_EQ(driver_getFileSize(test_file), expected_size);
+
+	//multifile _ one header line
+	constexpr const char* test_multifile_range = "gs://data-test-khiops-driver-gcs/khiops_data/split/Adult/Adult-split-0[0-5].txt";
+	constexpr long long expected_size_range{ 5634411 };
+	ASSERT_EQ(driver_getFileSize(test_multifile_range), expected_size_range);
+
+	//multifile _ header line at each file
+	constexpr const char* test_multifile_glob = "gs://data-test-khiops-driver-gcs/khiops_data/bq_export/Adult/*.txt";
+	constexpr long long expected_size_glob{ 5585576 };
+	ASSERT_EQ(driver_getFileSize(test_multifile_glob), expected_size_glob);
+
+	//mutifile _ one header _ double star glob pattern
+	constexpr const char* test_doublestar_glob = "gs://data-test-khiops-driver-gcs/khiops_data/split/Adult_subsplit/**/Adult-split-*.txt";
+	//sample has same size as multifile _ one header line
+	ASSERT_EQ(driver_getFileSize(test_doublestar_glob), expected_size_range);
 }
