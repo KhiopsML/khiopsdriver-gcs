298502d7a7089b2843ab789ef78a2ede38f87f8e
Cedric LE CAM <cedric.lecam@sii.fr>
2024-06-13 17:13:33 +0200

    Refactor tests.

--- test/basic_test.cpp
+++ test/basic_test.cpp
@@ -1,11 +1,16 @@
 #include <gtest/gtest.h>
 
+#include <array>
 #include <iostream>
 
 #include "../src/gcsplugin.h"
 
-constexpr int kSuccess{ 1 };
-constexpr int kFailure{ 0 };
+constexpr int kSuccess{ 0 };
+constexpr int kFailure{ 1 };
+
+constexpr int kFalse{ 0 };
+constexpr int kTrue{ 0 };
+
 
 TEST(GCSDriverTest, GetDriverName)
 {
@@ -24,35 +29,28 @@ TEST(GCSDriverTest, GetScheme)
 
 TEST(GCSDriverTest, IsReadOnly)
 {
-	ASSERT_FALSE(driver_isReadOnly());
+	ASSERT_EQ(driver_isReadOnly(), kTrue);
 }
 
 TEST(GCSDriverTest, Connect)
 {
 	//check connection state before call to connect
-	ASSERT_FALSE(driver_isConnected());
+	ASSERT_EQ(driver_isConnected(), kFalse);
 
 	//call connect and check connection
 	ASSERT_EQ(driver_connect(), kSuccess);
-	ASSERT_TRUE(driver_isConnected());
+	ASSERT_EQ(driver_isConnected(), kTrue);
 
 	//call disconnect and check connection
 	ASSERT_EQ(driver_disconnect(), kSuccess);
-	ASSERT_FALSE(driver_isConnected());
+	ASSERT_EQ(driver_isConnected(), kFalse);
 }
 
 TEST(GCSDriverTest, Disconnect)
 {
 	ASSERT_EQ(driver_connect(), kSuccess);
 	ASSERT_EQ(driver_disconnect(), kSuccess);
-	ASSERT_FALSE(driver_isConnected());
-}
-
-TEST(GCSDriverTest, GetFileSize)
-{
-	ASSERT_EQ(driver_connect(), kSuccess);
-	ASSERT_EQ(driver_getFileSize("gs://data-test-khiops-driver-gcs/khiops_data/samples/Adult/Adult.txt"), 5585568);
-	ASSERT_EQ(driver_disconnect(), kSuccess);
+	ASSERT_EQ(driver_isConnected(), kFalse);
 }
 
 TEST(GCSDriverTest, GetMultipartFileSize)
@@ -67,42 +65,73 @@ TEST(GCSDriverTest, GetSystemPreferredBufferSize)
 	ASSERT_EQ(driver_getSystemPreferredBufferSize(), 4 * 1024 * 1024);
 }
 
+constexpr const char* test_dir_name = "gs://data-test-khiops-driver-gcs/khiops_data/bq_export/Adult/";
+
+constexpr const char* test_single_file = "gs://data-test-khiops-driver-gcs/khiops_data/samples/Adult/Adult.txt";
+constexpr const char* test_range_file_one_header = "gs://data-test-khiops-driver-gcs/khiops_data/split/Adult/Adult-split-0[0-5].txt";
+constexpr const char* test_glob_file_header_each = "gs://data-test-khiops-driver-gcs/khiops_data/bq_export/Adult/*.txt";
+constexpr const char* test_double_glob_header_each = "gs://data-test-khiops-driver-gcs/khiops_data/split/Adult_subsplit/**/Adult-split-*.txt";
+
+constexpr std::array<const char*, 4> test_files = {
+	test_single_file,
+	test_range_file_one_header,
+	test_glob_file_header_each,
+	test_double_glob_header_each
+};
+
+
+class GCSDriverTestFixture : public ::testing::Test
+{
+	ASSERT_EQ(driver_connect(), kSuccess);
+	ASSERT_EQ(driver_getFileSize("gs://data-test-khiops-driver-gcs/khiops_data/samples/Adult/Adult.txt"), 5585568);
+	ASSERT_EQ(driver_disconnect(), kSuccess);
+}
 
 class GCSDriverTestFixture : public ::testing::Test {
 public:
 	static void SetUpTestSuite()
 	{
-		ASSERT_FALSE(driver_connect());
+		ASSERT_EQ(driver_connect(), kSuccess);
 	}
 
 	static void TearDownTestSuite()
 	{
-		ASSERT_FALSE(driver_disconnect());
+		ASSERT_EQ(driver_disconnect(), kSuccess);
 	}
 };
 
 TEST_F(GCSDriverTestFixture, FileExists)
 {
-	//single file
-	ASSERT_TRUE(driver_fileExists("gs://data-test-khiops-driver-gcs/khiops_data/bq_export/Adult/Adult-split-000000000000.txt"));
+	for (const char* file : test_files)
+	{
+		ASSERT_EQ(driver_fileExists(file), kTrue);
+	}
+	
+	ASSERT_EQ(driver_fileExists(test_dir_name), kFalse);
 
-	//file glob
-	ASSERT_TRUE(driver_fileExists("gs://data-test-khiops-driver-gcs/khiops_data/split/Adult_subsplit/**"));
+	ASSERT_EQ(driver_fileExists(nullptr), kFalse);
+}
 
-	//with dir
-	ASSERT_FALSE(driver_fileExists("gs://data-test-khiops-driver-gcs/khiops_data/bq_export/Adult/"));
+TEST_F(GCSDriverTestFixture, DirExists)
+{
+	ASSERT_EQ(driver_dirExists("any_name"), kTrue);
+
+	ASSERT_EQ(driver_dirExists(nullptr), kFalse);
 }
 
 TEST_F(GCSDriverTestFixture, GetFileSize)
 {
-	constexpr const char* test_file = "gs://data-test-khiops-driver-gcs/khiops_data/samples/Adult/Adult.txt";
-	constexpr long long expected_size{ 5585568 };
-	ASSERT_EQ(driver_getFileSize(test_file), expected_size);
+	constexpr std::array<long long, 4> expected_sizes = {
+		5585568,	// samples/Adult/Adult.txt
+		5634411,	// split/Adult/Adult-split-0[0-5].txt
+		5585576,	// bq_export/Adult/*.txt
+		5634411		// split/Adult_subsplit/**/Adult-split-*.txt
+	};
 
-	//multifile _ one header line
-	constexpr const char* test_multifile_range = "gs://data-test-khiops-driver-gcs/khiops_data/split/Adult/Adult-split-0[0-5].txt";
-	constexpr long long expected_size_range{ 5634411 };
-	ASSERT_EQ(driver_getFileSize(test_multifile_range), expected_size_range);
+	for (size_t i = 0; i < test_files.size(); i++)
+	{
+		ASSERT_EQ(driver_getFileSize(test_files[i]), expected_sizes[i]);
+	}
 
 	//multifile _ header line at each file
 	constexpr const char* test_multifile_glob = "gs://data-test-khiops-driver-gcs/khiops_data/bq_export/Adult/*.txt";
