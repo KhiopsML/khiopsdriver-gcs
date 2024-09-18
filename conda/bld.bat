1db180b9e194982fd2419258884669b76834f17c
bruno <97033386+bruno-at-orange@users.noreply.github.com>
2024-09-18 18:09:52 +0200

    Add conda packaging worflow

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,17 +1,11 @@
-#!/bin/bash
+REM Echo all output
+@echo on
 
-# Set-up the shell to behave more like a general-purpose programming language
-set -euo pipefail
-
-# Configure project
+REM Configure project
 cmake --fresh -G Ninja -D CMAKE_BUILD_TYPE=Release -D VCPKG_BUILD_TYPE=release -D CMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake -B builds/conda -S .
 
-# Build
+REM Build
 cmake --build builds/conda --target khiopsdriver_file_gcs
 
-# Copy binary to conda package
+REM Copy binary to conda package
 cmake --install builds/conda --prefix $PREFIX
-
-
-
-
