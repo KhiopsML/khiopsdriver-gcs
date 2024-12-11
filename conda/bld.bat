7d17d0562b45bdcb432adc7e98897d492db11ac4
Stephane Gouache <stephane.gouache@orange.com>
2024-12-11 17:55:05 +0100

    Fix windows installation

--- conda/bld.bat
+++ conda/bld.bat
@@ -21,4 +21,4 @@ REM Copy binary to conda package
 cmake --install builds/conda --prefix $PREFIX
 
 REM Copy the libs for the driver package
-copy build\conda\bin\khiopsdriver_file_gcs.dll %PREFIX%\lib
+copy bin\khiopsdriver_file_gcs.dll %PREFIX%\lib
