249ee9c13f8c1956210ef33524630010682a4e3d
Stephane Gouache <stephane.gouache@orange.com>
2024-12-12 19:46:42 +0100

    Fix conda packaging

--- conda/bld.bat
+++ conda/bld.bat
@@ -19,4 +19,4 @@ mkdir %PREFIX%\lib
 
 REM Copy the libs for the driver package
 copy builds\conda\bin\*.dll %PREFIX%\lib\
-ren %PREFIX%\lib\khiopsdriver_file_gcs.dll %PREFIX%\lib\libkhiopsdriver_file_gcs.dll
+move %PREFIX%\lib\khiopsdriver_file_gcs.dll %PREFIX%\lib\libkhiopsdriver_file_gcs.dll
