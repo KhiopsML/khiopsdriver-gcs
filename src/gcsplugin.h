9540d1bc2c20e83e0ff7c8e425ba86a5a329391c
Stephane Gouache <stephane.gouache@orange.com>
2025-01-10 18:54:56 +0100

    Remove rocky8

--- src/gcsplugin.h
+++ src/gcsplugin.h
@@ -2,7 +2,7 @@
 
 #define KHIOPS_STR(s) #s
 // Driver version
-#define DRIVER_VERSION KHIOPS_STR(0.1.0-rc.1)
+#define DRIVER_VERSION KHIOPS_STR(0.0.11)
 // Release versions must have 3 digits, for example KHIOPS_STR(1.2.0)
 // Alpha, beta ou release candidate have an extra suffix, for example :
 // - KHIOPS_STR(1.2.0-a.1)
