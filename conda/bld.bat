13abee7100636242467570d78ca2283cdc580941
Stéphane Gouache <stephane.gouache@orange.com>
2025-03-27 16:29:43 +0100

    Fix build

--- conda/bld.bat
+++ conda/bld.bat
@@ -9,7 +9,7 @@ W:
 vcpkg\vcpkg.exe install
 
 REM Configure project
-cmake --fresh -G Ninja -D CMAKE_BUILD_TYPE=Release -D VCPKG_BUILD_TYPE=release -D CMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake -B builds/conda -S .
+cmake --fresh -G Ninja -D CMAKE_BUILD_TYPE=Release -B builds/conda -S .
 
 REM Build
 cmake --build builds/conda --parallel --target khiopsdriver_file_gcs
