b06a95142d7e311c62c1534b011b7d0bd2cbe66d
Stéphane Gouache <stephane.gouache@orange.com>
2025-03-31 18:38:27 +0200

    Try fix build

--- conda/bld.bat
+++ conda/bld.bat
@@ -8,10 +8,10 @@ subst W: %CD%
 W:
 
 REM Configure project
-cmake --fresh -G Ninja -D CMAKE_BUILD_TYPE=Release -B builds/conda -S .
+cmake --fresh -G Ninja -D CMAKE_BUILD_TYPE=Release -B builds\conda -S .
 
 REM Build
-cmake --build builds/conda --parallel --target khiopsdriver_file_gcs
+cmake --build builds\conda --parallel --target khiopsdriver_file_gcs
 
 REM Create drivers installation directory
 mkdir %PREFIX%\bin
